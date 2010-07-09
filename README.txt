ad40cc89b354c6dd24748c71d801bed10ed05ba2
Hanno Schlichting <hanno@hannosch.eu>
2010-07-09 19:54:46 +0000

    Boilerplate

---
+++ README.txt
@@ -0,0 +1,6 @@
+Overview
+========
+
+The Products.ZSQLMethods product provides support for SQL Method objects in
+Zope 2. They can be used in conjunction with any database adapter to use
+relational database data from within the Zope environment.
---
+++ setup.py
@@ -0,0 +1,34 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(name='Products.ZSQLMethods',
+      version = '2.13.0dev',
+      url='http://pypi.python.org/pypi/Products.ZSQLMethods',
+      license='ZPL 2.1',
+      description="SQL method support for Zope 2.",
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      long_description=open('README.txt').read() + '\n' +
+                       open('CHANGES.txt').read(),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=[
+        'setuptools',
+        'Zope2 >= 2.13.0a1',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      )
