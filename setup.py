de21aba9fcb817cb00b79f2c3da44785032d05a2
Tres Seaver <tseaver@palladion.com>
2014-12-18 13:50:33 -0500

    Resolve version conflict.
    Record needs EC >= 4.1a1, but Acquisition needs EC >= 4.0a1.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'setuptools',
         'Acquisition',
         'DateTime',
-        'ExtensionClass',
+        'ExtensionClass>=4.1a1',
         'Missing',
         'Persistence',
         'Record',
