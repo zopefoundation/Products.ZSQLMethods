14e6715b334263ac46215b854a1d7762ef4891e6
Jens Vagelpohl <jens@netz.ooo>
2017-05-23 09:05:28 +0200

    - use released Zope version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     install_requires=[
         'setuptools',
         'six',
-        'Zope2 > 4.0a4',
+        'Zope2 >= 4.0a5',
         'RestrictedPython >= 4.0dev',
         'AccessControl >= 4.0dev',
         'Persistence >= 3.0dev',
